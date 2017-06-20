b1da302846ae5b2cf347f14455c057125a749d2c
jaxx2104 <jaxx2104@gmail.com>
2017-06-21 08:56:39 +0900

    ログを追加

---
+++ src/services/log.js
@@ -0,0 +1,33 @@
+import log4js from 'log4js'
+
+log4js.configure({
+  'appenders': [
+    {
+      'type':     'dateFile',
+      'category': 'system',
+      'filename': 'logs/system.log',
+      'pattern':  '-yyyy-MM-dd'
+    },
+    {
+      'type':     'dateFile',
+      'category': 'access',
+      'filename': 'logs/access.log',
+      'pattern':  '-yyyy-MM-dd'
+    },
+    {
+      'type':     'dateFile',
+      'category': 'error',
+      'filename': 'logs/error.log',
+      'pattern':  '-yyyy-MM-dd'
+    }
+  ],
+  categories: {
+    default: {
+      appenders: ['system'],
+      level: 'info'
+    }
+  }
+})
+
+const log = log4js.getLogger('system')
+export default log
